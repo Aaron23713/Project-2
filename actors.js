47fb1f667b434d1fc52b06a05769ef5c92d9c277
buitx <buitx@dukes.jmu.edu>
2022-12-09 02:23:38 -0500

    ha

--- actors.js
+++ actors.js
@@ -1,15 +1,6 @@
 const queryInputElem = document.getElementById('query');
 const frigginForm = document.getElementById('vestigial');
 var img;
-function myFunction() {
-  var x = document.getElementById("vestigial");
-  var text = "";
-  var i;
-  for (i = 0; i < x.length ;i++) {
-    text += x.elements[i].value + "<br>";
-  }
-  document.getElementById("demo").innerHTML = text;
-}
 
 const options = {
 	method: 'GET',
@@ -33,11 +24,25 @@ queryInputElem.addEventListener('keyup', async function(ev) {
   if (ev.key == 'Enter') {
     console.log('pressed enter')
 
-    fetch(`https://movie-database-alternative.p.rapidapi.com/?s=${queryInputElem.value}&r=json&page=1`, options)
+    const movie = await fetch(`https://movie-database-alternative.p.rapidapi.com/?s=${queryInputElem.value}&r=json&page=1`, options)
 	    .then(response => response.json())
-	    .then(response => document.write(img src = "response.Search[0].imdbID"))
-      // .then(response => console.log(response.Search[0].Poster))
+      .then(response => { console.log(response.Search); get_data(response.Search) })
+      // .then(response => get_data(response.Search))
 	    .catch(err => console.error(err));
-  
+    
   }
 });
+
+function myFunction() {
+  var x = document.getElementById("vestigial");
+  var text = "";
+  var i;
+  for (i = 0; i < x.length ;i++) {
+    text += x.elements[i].value + "<br>";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+
+function get_data(data) {
+  const movies = data;
+}
