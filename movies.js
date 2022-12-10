af612eb95d9f7c43e22daaac573c0348e2f2909e
buitx <buitx@dukes.jmu.edu>
2022-12-09 19:51:39 -0500

    hello

--- movies.js
+++ movies.js
@@ -8,4 +8,32 @@ for (var i = 0; i < movies.length; i++) {
     movie.append(imgs);
 }
 
+function secondApiInfo(secondData) {
 
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'b70d74c746msh2b5feafa0807d8ap1af83djsn8d2a39a09d64',
+        'X-RapidAPI-Host': 'movie-details1.p.rapidapi.com'
+      }
+    };
+    
+    fetch(`https://movie-details1.p.rapidapi.com/imdb_api/movie?id=${secondData}`, options)
+      .then(response => response.json())
+      .then(response => {console.log(response); get_data2(response) })
+      .catch(err => console.error(err));
+      
+  }
+    
+  function get_data2(data) { 
+  
+  
+  var meety = data;
+  var oImg11;
+  oImg11 = document.createElement("img");
+  oImg11.setAttribute('src',meety.image);
+  console.log(meety.image);
+  document.body.append(oImg11);
+  document.body.append(meety.description);
+  
+  }
